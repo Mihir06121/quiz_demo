05f56f62a83602d297983da6a215bffc13a81ddc
Mihir <mp06121@gmail.com>
2023-11-24 00:05:07 +0530

    typed-based results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { Collapse } from 'reactstrap'
 // import { FullScreen, useFullScreenHandle } from "react-full-screen";
